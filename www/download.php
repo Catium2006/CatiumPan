91d00b1b6928e272851842a39f37a880ea5679b8
Tony Shang <64829162+Catium2006@users.noreply.github.com>
2022-10-13 10:30:48 +0800

    带计数的下载

--- www/download.php
+++ www/download.php
@@ -3,19 +3,30 @@ include('db.php');
 if ($_GET['file_id']) {
     $file_id = $_GET['file_id'];
     $arr = exec_sql("SELECT * FROM files WHERE file_id = '$file_id'");
+    $user_uuid = $_COOKIE['uuid'];
     if (count($arr) > 0) {
         $row = $arr[0];
         $file_res = $row['file_res'];
         $file_name = $row['file_name'];
-        $file_size = filesize('res/' . $file_res);
-        header("Content-type: application/octet-stream");
-        header("Accept-Ranges: bytes");
-        header("Accept-Length: " . $file_size);
-        header("Content-Disposition: attachment; filename=" . $file_name);
-        $file = fopen('res/' . $file_res, "rb");
-        echo fread($file, $file_size);
-        fclose($file);
+        $file_max_download_count = $row['file_max_download_count'];
+        $file_download_count = $row['file_download_count'];
+        if ($file_download_count >= $file_max_download_count) {
+            exec_sql("DELETE FROM files WHERE file_id = '$file_id'");
+        } else {
+            exec_sql("UPDATE files SET file_download_count = file_download_count + 1 WHERE file_id = '$file_id'");
+            exec_sql("UPDATE users SET user_download_count = user_download_file_count + 1 WHERE user_uuid = '$user_uuid'");
+            $file_size = filesize('res/' . $file_res);
+            header("Content-type: application/octet-stream");
+            header("Accept-Ranges: bytes");
+            header("Accept-Length: " . $file_size);
+            header("Content-Disposition: attachment; filename=" . $file_name);
+            $file = fopen('res/' . $file_res, "rb");
+            echo fread($file, $file_size);
+            fclose($file);
+        }
     } else {
         header('HTTP/1.1 404 NOT FOUND');
     }
+} else {
+    header('HTTP/1.1 404 NOT FOUND');
 }
--- www/index.php
+++ www/index.php
@@ -13,7 +13,9 @@ if (is_logined()) {
     <p>已登录</p>
     <form action="upload.php" method="post" enctype="multipart/form-data">
         <label for="file">文件：</label>
-        <input type="file" name="file" id="file"><br>
+        <input type="file" name="file"><br>
+        <label for="file">限制下载次数：</label>
+        <input type="number" name="file_max_download_count" value="1"><br>
         <input type="submit" name="submit" value="提交">
     </form>
 <?php
